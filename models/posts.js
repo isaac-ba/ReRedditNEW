02f152a838550dca6ea6bce9c907fec5c1979eeb
Isaac BA <iba.burton@gmail.com>
2016-12-27 16:25:19 +0200

    client-side

--- models/posts.js
+++ models/posts.js
@@ -4,9 +4,11 @@ var PostSchema = new mongoose.Schema({
   title: String,
   link: String,
   upvotes: {type: Number, default: 0},
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comments' }]
 });
-
+// PostSchema.methods.upVote = function(){
+//   this.upvotes++;
+// }
 var Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var Post = require('.././models/posts');
+var Comment = require('.././models/comments');
 
 router.get('/', function(req, res, next) {
   res.send('this is from /!');
@@ -29,7 +30,19 @@ router.param('post', function(req, res, next, id) {
 });
 
 router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
 
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
 });
 
 router.get('/posts',function(req,res){
@@ -37,6 +50,37 @@ router.get('/posts',function(req,res){
     res.send(posts);
   });
 }); 
+
+router.get('/posts/:post/comments', function(req,res){
+  console.log(req.post._id);
+  // Post.findOne({_id: req.post._id}).populate('comments').exec(function(error,post){
+  //   console.log(post);
+  // })
+  console.log(req.post)
+  req.post.populate('comments', function(err, post){
+    res.send(post);
+  });
+  // req.post.populate('comments').exec(function(err, post){
+  //   res.send(post);
+  // });
+})
+// router.put
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote();
+
+
+  req.post.save(function(err, post) {
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote();
+
+  req.comment.save(function(err, comment) {
+    res.json(comment);
+  });
+});
 
 module.exports = router;
 
